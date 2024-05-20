76db8beadcfd020700bd4852f8d9fd845a4a7384
Sakshi Singh <sakshisingh3209@gmail.com>
2024-05-21 00:37:57 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log("listening on Port 3000");
+})
